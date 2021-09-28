bc6bbf3f2d50f4194703d56fce1b461931ad3e63
Pavel Quevedo <pavelquevedo@outlook.com>
2021-09-28 12:20:57 -0600

    -Add auth guard for main routes.

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -4,13 +4,21 @@ import { HomeComponent } from './home/home.component';
 import { MemberDetailComponent } from './member/member-detail/member-detail.component';
 import { MemberListComponent } from './member/member-list/member-list.component';
 import { MessagesComponent } from './messages/messages.component';
+import { AuthGuard } from './_guards/auth.guard';
 
 const routes: Routes = [
   {path:'', component: HomeComponent},
-  {path:'members', component: MemberListComponent},
-  {path:'members/:id', component: MemberDetailComponent},
-  {path:'lists', component: MemberListComponent},
-  {path:'messages', component: MessagesComponent},
+  {
+    path:'',
+    runGuardsAndResolvers: 'always',
+    canActivate: [AuthGuard],
+    children:[
+      {path:'members', component: MemberListComponent, canActivate:[AuthGuard]},
+      {path:'members/:id', component: MemberDetailComponent},
+      {path:'lists', component: MemberListComponent},
+      {path:'messages', component: MessagesComponent}
+    ]
+  }, 
   {path:'**', component: HomeComponent, pathMatch:'full'}
 ];
 
