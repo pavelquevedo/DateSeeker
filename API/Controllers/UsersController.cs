8b393ca0080fb5d9ef43f227ce872e2f6e176c68
Pavel Quevedo <pavelquevedo@outlook.com>
2021-10-25 16:22:44 -0600

    Apply repository pattern, add Member and Photo dtos, add AutoMapper configuration.

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -2,7 +2,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using API.Data;
+using API.DTOs;
 using API.Entities;
+using API.Interfaces;
+using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -12,32 +15,46 @@ namespace API.Controllers
     [Authorize]
     public class UsersController : BaseApiController
     {
-        private readonly DataContext context;
+        private readonly IUserRepository _userRepository;
+        private readonly IMapper _mapper;
 
-        public UsersController(DataContext context)
+        public UsersController(IUserRepository userRepository, IMapper mapper)
         {
-            this.context = context;
+            this._userRepository = userRepository;
+            this._mapper = mapper;
         }
 
         // api/users
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers(){
+        public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers(){
+            
+            var users = await _userRepository.GetMembersAsync();
 
-            var users = await this.context.Users.ToListAsync(); 
+            //This is another option to return mapped objects, but is heavier
+            //var usersToReturn = _mapper.Map<IEnumerable<MemberDto>>(users);
 
-            return users;
+            return Ok(users);
         }
 
         // api/users/3
-        [HttpGet("{id}")]
-        public async Task<ActionResult<AppUser>> GetUsers(int id){
-            
-            var user = await this.context.Users.FindAsync(id);
-        
-            return user;
+        //[HttpGet("{id}")]
+        //public async Task<ActionResult<MemberDto>> GetUsers(int id){
+
+        //    var user = await _userRepository.GetUserByIdAsync(id);
+
+        //    return _mapper.Map<MemberDto>(user);
+        //}
+
+        // api/users/daniel
+        [HttpGet("{username}")]
+        public async Task<ActionResult<MemberDto>> GetUsers(string username)
+        {
+
+            var user = await _userRepository.GetUserByUserNameAsync(username);
+
+            return _mapper.Map<MemberDto>(user);
         }
 
-        
 
 
     }
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,19 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {HttpClientModule} from '@angular/common/http'
+import {HttpClientModule, HTTP_INTERCEPTORS} from '@angular/common/http'
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations'
 import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
-import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
 import { MemberDetailComponent } from './member/member-detail/member-detail.component';
 import { MemberListComponent } from './member/member-list/member-list.component';
 import { MessagesComponent } from './messages/messages.component'
-import { ToastrModule } from 'ngx-toastr';
+import { SharedModule } from './_modules/shared.module';
+import { TestErrorsComponent } from './errors/test-errors/test-errors.component';
+import { ErrorInterceptor } from './_interceptors/error.interceptor';
+import { NotFoundComponent } from './errors/not-found/not-found.component';
+import { ServerErrorComponent } from './errors/server-error/server-error.component';
 
 
 @NgModule({
@@ -24,7 +27,10 @@ import { ToastrModule } from 'ngx-toastr';
     RegisterComponent,
     MemberDetailComponent,
     MemberListComponent,
-    MessagesComponent
+    MessagesComponent,
+    TestErrorsComponent,
+    NotFoundComponent,
+    ServerErrorComponent
   ],
   imports: [
     BrowserModule,
@@ -32,12 +38,11 @@ import { ToastrModule } from 'ngx-toastr';
     HttpClientModule,
     BrowserAnimationsModule,
     FormsModule,
-    BsDropdownModule.forRoot(),
-    ToastrModule.forRoot({
-      positionClass:'toast-bottom-right'
-    })
+    SharedModule  
   ],
-  providers: [],
+  providers: [
+    {provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi:true}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
