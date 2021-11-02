c89e1665736c82443d1947ce55d4d3a89727ab65
Pavel Quevedo <pavelquevedo@outlook.com>
2021-11-01 19:51:56 -0600

    -Add reactive form and validation.

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {HttpClientModule, HTTP_INTERCEPTORS} from '@angular/common/http'
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations'
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -22,6 +22,7 @@ import { JwtInterceptor } from './_interceptors/jwt.interceptor';
 import { MemberEditComponent } from './member/member-edit/member-edit.component';
 import { LoadingInterceptor } from './_interceptors/loading.interceptor';
 import { PhotoEditorComponent } from './member/photo-editor/photo-editor.component';
+import { TextInputComponent } from './_forms/text-input/text-input.component';
 
 
 @NgModule({
@@ -38,7 +39,8 @@ import { PhotoEditorComponent } from './member/photo-editor/photo-editor.compone
     ServerErrorComponent,
     MemberCardComponent,
     MemberEditComponent,
-    PhotoEditorComponent
+    PhotoEditorComponent,
+    TextInputComponent
   ],
   imports: [
     BrowserModule,
@@ -46,7 +48,8 @@ import { PhotoEditorComponent } from './member/photo-editor/photo-editor.compone
     HttpClientModule,
     BrowserAnimationsModule,
     FormsModule,
-    SharedModule    
+    SharedModule,
+    ReactiveFormsModule 
   ],
   providers: [
     {provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi:true},
--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { AbstractControl, FormControl, FormGroup, ValidatorFn, Validators } from '@angular/forms';
 import { ToastrService } from 'ngx-toastr';
 import { AccountService } from '../_services/account.service';
 
@@ -18,17 +19,49 @@ export class RegisterComponent implements OnInit {
     password:""
   }
 
+  
+
+  registerForm: FormGroup;
+
   ngOnInit(): void {
+    this.initializeForm();
+  }
+
+  getObjectKeys(arg: any) {
+    return Object.keys(arg);
+  }
+
+  initializeForm(){
+    this.registerForm = new FormGroup({
+      username: new FormControl('', Validators.required),
+      password: new FormControl('', [Validators.required, Validators.minLength(4), Validators.maxLength(8)]),
+      confirmPassword: new FormControl('', [Validators.required, this.matchValues('password')])
+    });
+    //This is to re-fire the validation of confirmPassword if the user
+    // changes the password field after confirmPassword
+    this.registerForm.controls.password.valueChanges.subscribe(()=>{
+      this.registerForm.controls.confirmPassword.updateValueAndValidity();
+    })
+  }
+
+  matchValues(matchTo: string): ValidatorFn{
+    return(control: AbstractControl) =>{
+      const parentControls = control.parent?.controls as any;
+      return (parentControls) 
+        ? (control?.value === parentControls[matchTo]?.value) 
+        ? null : {isMatching: true} : null;
+    }
   }
 
   register(){
-    this.accountService.register(this.model).subscribe(response =>{
-      console.log(response);
-      this.cancel();
-    }, error => {
-      console.log(error);
-      this.toastr.error(error.error);
-    });
+    console.log(this.registerForm.value);
+    // this.accountService.register(this.model).subscribe(response =>{
+    //   console.log(response);
+    //   this.cancel();
+    // }, error => {
+    //   console.log(error);
+    //   this.toastr.error(error.error);
+    // });
   }
 
   cancel(){
