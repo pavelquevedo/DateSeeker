5528d6e4b0826862e0020600abb32f9760d7a79c
Pavel Quevedo <pavel.quevedo@rivetlogic.com>
2022-04-08 16:59:56 -0600

    Adding member caching to the frontend, remembering the filters for a user in the service.

--- client/src/app/member/member-list/member-list.component.ts
+++ client/src/app/member/member-list/member-list.component.ts
@@ -21,11 +21,15 @@ export class MemberListComponent implements OnInit {
   user: User;
   genderList = [{value: 'male', display: 'Males'}, {value: 'female', display: 'females'}];
 
-  constructor(private memberService: MembersService, private accountService: AccountService) {
-    this.accountService.currentUser$.pipe(take(1)).subscribe(user => {
-      this.user = user;
-      this.userParams = new UserParams(user);
-    })
+  constructor(private memberService: MembersService) {
+    //This was moved to members.service.ts in order to cache user params selection   
+    // this.accountService.currentUser$.pipe(take(1)).subscribe(user => {
+    //   this.user = user;
+    //   this.userParams = new UserParams(user);
+    // })
+
+    //This is the new way to get cached params, because services are singleton so the selection will remain during the application lifecycle
+    this.userParams = this.memberService.getUserParams();
    }
 
   ngOnInit(): void {
@@ -34,6 +38,9 @@ export class MemberListComponent implements OnInit {
   }
 
   loadMembers(){
+    //Caching params selection
+    this.memberService.setUserParams(this.userParams);    
+    
     this.memberService.getMembers(this.userParams).subscribe(response =>{
       this.members = response.result;
       this.pagination = response.pagination;
@@ -41,12 +48,19 @@ export class MemberListComponent implements OnInit {
   }
 
   resetFilters(){
-    this.userParams = new UserParams(this.user);
+    //Old way to reset user params
+    //this.userParams = new UserParams(this.user);
+
+    //New way to reset user params from the service
+    this.userParams = this.memberService.resetUserParams();
+
     this.loadMembers();
   }
 
   pageChanged(event: any){
     this.userParams.pageNumber = event.page;
+    //Caching params since the selection changed        
+    this.memberService.setUserParams(this.userParams);
     this.loadMembers();
   }
 
